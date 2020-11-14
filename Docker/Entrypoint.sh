9fe60e5625c65ace9f6bea574023a71921b07d5c
EvineDeng <evinedeng@foxmail.com>
2020-11-14 10:40:08 +0800

    调整目录

--- Docker/Entrypoint.sh
+++ Docker/Entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-## 修改日期：2020-10-17
+## 修改日期：2020-11-14
 ## 作者：Evine Deng <evinedeng@foxmail.com>
 
 set -e
@@ -14,11 +14,7 @@ crond -L /root/log/crond.log
 URLFirstRun="https://raw.githubusercontent.com/EvineDeng/jd-base/main/first_run.sh"
 
 cd /root
-sh -c "$(wget ${URLFirstRun} -O -)"
-if [ $? -ne 0 ]
-then
-  sh -c "$(curl -fsSL ${URLFirstRun})"
-fi
+sh -c "$(wget ${URLFirstRun} -O -)" || sh -c "$(curl -fsSL ${URLFirstRun})"
 
 
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
