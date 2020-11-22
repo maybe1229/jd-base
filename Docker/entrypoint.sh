f066acbfa4b6750d6ef0adc8cd1c10c4a8ea06c4
EvineDeng <evinedeng@foxmail.com>
2020-11-22 13:17:15 +0800

    优化docker的entrypoint

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 set -e
 
-if [ ! -d /root/log ]; then
-  mkdir -p /root/log
-fi
-
+[ ! -d /root/log ] && mkdir -p /root/log
 crond -L /root/log/crond.log
-
-if [ ! -f /root/first_run.sh ]
-then
-  cp -f /first_run.sh /root/first_run.sh
-  chmod 777 /root/first_run.sh
-else
-  FirstRunOld=$(cat /root/first_run.sh)
-  FirstRunNew=$(cat /first_run.sh)
-  if [ "${FirstRunOld}" != "${FirstRunNew}" ]; then
-    cp -f /first_run.sh /root/first_run.sh
-    chmod 777 /root/first_run.sh
-  fi
-fi
+cp -f /first_run.sh /root/first_run.sh && chmod 777 /root/first_run.sh
 
 cd /root
-
 if [ ! -d /root/scripts ] || [ ! -d /root/shell ] || [ ! -d /root/log ] || [ -f /root/crontab.list ]; then
   bash first_run.sh
 fi
