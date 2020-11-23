18e632c3ba43f5755ed294bde6bfe9624274334b
EvineDeng <evinedeng@foxmail.com>
2020-11-23 16:15:29 +0800

    增加每日签到接口延迟时间自定义功能；增加每日签到关闭通知和简洁通知的控制开关；增加User-Agent自定义功能。如需要使用上述功能，需要使用新的git_pull.sh

--- git_pull_function.sh
+++ git_pull_function.sh
@@ -451,7 +451,7 @@ function Set_NotifyBeanSign {
       for file in ${ListShellDir}
       do
         perl -i -pe "s|^.*(export JD_BEAN_SIGN_STOP_NOTIFY=).*$|\1true|" ${file}
-        perl -i -pe "s|^.*(export JD_BEAN_SIGN_NOTIFY_SIMPLE=).*$|# \1" ${file}
+        perl -i -pe "s|^.*(export JD_BEAN_SIGN_NOTIFY_SIMPLE=).*$|# \1|" ${file}
       done
       ;;
     1)
