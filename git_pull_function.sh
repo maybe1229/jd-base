27fc75d386abef4ef8139c6026bb5d5bfbcd6f23
EvineDeng <evinedeng@foxmail.com>
2020-11-29 12:02:18 +0800

    修改定义User-Agent会报错的bug

--- git_pull_function.sh
+++ git_pull_function.sh
@@ -534,12 +534,12 @@ function Set_UserAgent {
     echo -e "设置User-Agent为 ${UserAgent}\n\n仅在运行 shell 脚本时有效，直接运行 js 脚本无效...\n"
     for file in ${ListShellDir}
     do
-      perl -i -pe "s|^.*(export JD_USER_AGENT=).*$|\1\"${UserAgent}\"|" ${file}
+      perl -i -pe "s<.*(export JD_USER_AGENT=).*$><\1\"${UserAgent}\">" ${file}
     done
   else
     for file in ${ListShellDir}
     do
-      perl -i -pe "s|^.*(export JD_USER_AGENT=).*$|# \1|" ${file}
+      perl -i -pe "s<.*(export JD_USER_AGENT=).*$><# \1>" ${file}
     done
   fi
 }
